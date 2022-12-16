013ed2699cfef00ecea86b6f4cb33b9bc709171a
Ducc <minhduck3359@gmail.com>
2022-12-16 17:34:25 +0700

    eap

--- eap.h
+++ eap.h
@@ -1,3 +1,13 @@
+/*
+An implementation of the paper
+"Erasing Appearance Preservation in Optimization-based Smoothing"
+(European Conference on Computer Vision (ECCV) 2020)
+
+Please check their github page for more detail (https://github.com/lllyasviel/AppearanceEraser)
+
+Please refer to main.c to see how the wrapper works
+*/
+
 int eap(unsigned char * image, 
     int height, int width,
     double u,
